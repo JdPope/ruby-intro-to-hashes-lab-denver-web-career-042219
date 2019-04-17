2135b013caa228939fe43dd6aa2c5532950f741e
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-17 15:14:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ end
 
 def monopoly
 monopoly =  {
-  :railroads=>{:pieces=>4}
+  :railroads=>{
 }
  
   
@@ -18,7 +18,7 @@ end
 
 def monopoly_with_second_tier
 monopoly =  {
-  :railroads=>{}
+  :railroads=>{ :pieces=>4}
 }
 
 end
